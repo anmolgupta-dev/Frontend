ea14b9c62fcf977fdae204dd3d2fc6c18c91adeb
Pankaj Jain <pankaj.jain@daffodilsw.com>
2020-11-20 21:10:13 +0530

    Update Code

---
+++ pages/filterComponent.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+import { Constants } from '../util/constants';
+
+function FilterComponent({title, filterRoute, showMore}) {
+  let [filterData, setFilterData] = React.useState('');
+
+  const fetchData = React.useCallback(() => {
+    axios({
+      "method": "GET",
+      "url": `${Constants.WEB_SERVICE_URL}${filterRoute}`,
+    })
+    .then((response) => {
+      setFilterData(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  React.useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  const renderFilterData = (filterData) => {
+    if(!filterData) {
+      return;
+    }
+    const jobDetailItems = filterData.map(filterItem => {
+      return (
+          <div key={filterItem.key}>
+            <span>{filterItem.key}</span><span>{filterItem.doc_count}</span>
+          </div>
+      )
+    })
+    return jobDetailItems;
+  }
+
+  return (
+    <div className="App">
+      <div>{title}</div>
+      {renderFilterData(filterData)}
+    </div>
+  );
+}
+
+export default FilterComponent;
---
+++ util/constants.js
@@ -0,0 +1,18 @@
+export const Constants = {
+  WEB_SERVICE_URL : "http://localhost:5000",
+  WEB_SERVICE_ROUTES : {
+    JOB_TYPES: '/jobTypes',
+    WORK_SCHEDULES: '/workSchedules',
+    EXPERIENCES: '/experiences',
+    DEPARTMENTS: '/departments',
+    ALL_DEPARTMENTS: '/departments/all',
+    JOBS: '/jobs',
+    JOB_ITEMS: '/job/items'
+  },
+  FILTER_TITLE: {
+    JOB_TYPE: 'JOB TYPE',
+    DEPARTMENT: 'DEPARTMENT',
+    WORK_SCHEDULE: 'WORK SCHEDULE',
+    EXPERIENCE: 'EXPERIENCE',
+  }
+}
